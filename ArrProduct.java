697b0186380c7dc97e88b9cdaf36079660f45e8c
saimanojratnakaram <70759776+saimanojratnakaram@users.noreply.github.com>
2020-09-14 16:16:19 +0530

    Add files via upload
    Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
    
    For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].

---
+++ ArrProduct.java
@@ -0,0 +1,41 @@
+package Dailycoding;
+
+import java.util.Scanner;
+
+public class ArrProduct {
+     static void productSol(int arr[]) {
+         int n=arr.length;
+        int i, temp = 1;
+        int[] product = new int[n];
+        for(i=0;i<n;i++){
+            product[i]=1;
+        }
+        product[0]=arr[0];
+        for(i=1;i<n;i++){
+            product[i]=product[i-1]*arr[i];
+        }
+        for(i=n-1;i>0;i--){
+            product[i]=product[i-1]*temp;
+            temp*=arr[i];
+        }
+        product[0]=temp;
+        for(i=0;i<n;i++){
+            System.out.print(product[i]+" ");
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        String s= scanner.nextLine();
+        String[] s1 = s.split(" ");
+        int[] arr=new int[s1.length];
+        for(int i=0;i<s1.length;i++)
+        {
+            arr[i]=Integer.parseInt(s1[i]);
+        }
+        productSol(arr);
+    }
+    }
