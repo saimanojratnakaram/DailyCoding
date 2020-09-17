94eb77e77fafecd653474826059dd5ba6f9e8166
saimanojratnakaram <70759776+saimanojratnakaram@users.noreply.github.com>
2020-09-17 12:09:34 +0530

    Add files via upload
    DailyCoding Problems

---
+++ Problem8.java
@@ -0,0 +1,72 @@
+package Dailycoding;
+
+class Node{
+    int value;
+    Node left,right;
+    public Node(int item){
+        value=item;
+        left=right=null;
+    }
+
+}
+public class Problem8 {
+
+
+}
+
+class BinaryTree {
+    Node root;
+
+    BinaryTree(int key) {
+        root = new Node(key);
+    }
+
+    BinaryTree() {
+        root = null;
+    }
+
+    public static void main(String[] args) {
+
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(0);
+        tree.root.left = new Node(1);
+        tree.root.right = new Node(0);
+        tree.root.right.left = new Node(1);
+        tree.root.right.right = new Node(0);
+        tree.root.right.left.left = new Node(1);
+        tree.root.right.left.right = new Node(1);
+
+       int result= count_unival(tree.root);
+        System.out.println(result);
+
+    }
+
+    static boolean is_unival(Node root){
+        if(root==null){
+            return true;
+        }
+        if(root.left!=null && root.left.value!= root.value){
+            return false;
+        }
+        if(root.right!=null && root.right.value!= root.value){
+            return false;
+        }
+        if(is_unival(root.left) && is_unival(root.right)){
+            return true;
+        }
+        return false;
+
+    }
+    static int count_unival(Node root){
+        int total;
+        if(root==null){
+            return 0;
+        }
+        total=count_unival(root.left)+count_unival(root.right);
+        if(is_unival(root)){
+            total++;
+        }
+        return total;
+    }
+}
+
